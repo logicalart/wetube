24c93cf52eeecfb4bb614bcf4c090526c5abc070
Junmo Yang <jmyang@logicalart.co.kr>
2020-04-20 17:54:56 +0900

    #0001 app.listen(PORT, handleListening);

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi');
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res, next) => res.send("Hello from my ass");
+
+const handleProfile = (req, res, next) => res.send("You are on my profile");
+
+const beetweenHome = (req, res, next) => {
+    console.log("I'm beetween");
+    next();
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use(beetweenHome);
+
+app.listen(PORT, handleListening);
