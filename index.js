f253b40bd13e0141b68162b1e36c766d6712c52e
Junmo Yang <jmyang@logicalart.co.kr>
2020-04-21 17:00:10 +0900

    #0002 #2 ExpressJS

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -6,19 +10,18 @@ const PORT = 4000;
 const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res, next) => res.send("Hello from my ass");
+const handleHome = (req, res) => res.send("Hello from my ass");
 
-const handleProfile = (req, res, next) => res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-const beetweenHome = (req, res, next) => {
-    console.log("I'm beetween");
-    next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
-
-app.use(beetweenHome);
 
 app.listen(PORT, handleListening);
